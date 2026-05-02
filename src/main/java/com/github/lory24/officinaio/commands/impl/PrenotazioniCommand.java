9d004b11f8b2b928e004ebf136d3fa97faab8deb
Lorenzo Rocca <lorenzo.rocca.work@gmail.com>
2026-05-02 20:22:00 +0200

    Aggiunto messaggio di ingresso nel sotto-sistema e impl. di comandi

--- src/main/java/com/github/lory24/officinaio/commands/impl/PrenotazioniCommand.java
+++ src/main/java/com/github/lory24/officinaio/commands/impl/PrenotazioniCommand.java
@@ -11,6 +11,9 @@ public class PrenotazioniCommand implements Command {
 
     @Override
     public void execute(Officina officina, @NonNull ConsoleProvider provider, String[] args) {
+        System.out.println("\nAccesso eseguito alla modalita' di gestione delle prenotazioni.");
+        System.out.println("Per uscire, digita 'confirm'.\n");
+
         String input;
         while (true) {
             provider.printInputLine("prenotazioni");
@@ -22,12 +25,27 @@ public class PrenotazioniCommand implements Command {
             }
 
             // Altrimenti vedi in base al caso
-            switch (input.toLowerCase(Locale.ROOT)) {
+            switch (input.toLowerCase(Locale.ROOT).split(" ")[0]) {
                 // Comando di aiuto
                 case "help":
                 case "?": {
                     this.printHelpForm();
                     break;
+                }
+
+                // Comando per visualizzare le prenotazioni
+                case "view": {
+                    break;
+                }
+
+                // Comando per creare una prenotazione
+                case "create": {
+
+                }
+
+                // Comando per eliminare
+                case "delete": {
+
                 }
 
                 // Comando vuoto
