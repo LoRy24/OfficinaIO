9b227711f81be35428b5da859fd67b5886dfba2d
Lorenzo Rocca <lorenzo.rocca.work@gmail.com>
2026-05-09 23:11:26 +0200

    Creato il ToString per il Servizio e risolto bug con il totale

--- src/main/java/com/github/lory24/officinaio/core/Servizio.java
+++ src/main/java/com/github/lory24/officinaio/core/Servizio.java
@@ -20,7 +20,7 @@ public class Servizio {
     }
 
     public double calcolaCosto() {
-        double totale = 0.0f;
+        double totale = tipoServizio.getPrezzoServizio();
         for (Operazione operazione: operazioni)
             totale += operazione.prezzo;
 
@@ -28,5 +28,24 @@ public class Servizio {
     }
 
     // Tipo per le operazioni
-    public record Operazione(String descrizione, double prezzo) { }
+    public record Operazione(String descrizione, double prezzo) {
+        @Override
+        public String toString() {
+            return descrizione + " -> " + prezzo;
+        }
+    }
+
+    @Override
+    public String toString() {
+        // Crea la lista delle operazioni
+        StringBuilder operationsBuilder = new StringBuilder();
+        for (Operazione operazione: operazioni)
+            operationsBuilder.append("- ").append(operazione.toString()).append(" Euro").append("\n");
+
+        return "Tipo: " +  this.tipoServizio.getTipo() + "\n" +
+                "Prezzo base: " + this.tipoServizio.getPrezzoServizio() + " Euro\n" +
+                "Veicolo:\n" + this.veicolo.toString() +
+                "Operazioni:\n" + operationsBuilder +
+                "TOTALE SERVIZIO: " + this.calcolaCosto() + " Euro\n";
+    }
 }
